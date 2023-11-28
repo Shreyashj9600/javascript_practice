b15116d6e1914d5201d2e3b38e5eace497fc6464
Shreyashj9600 <connectshreyashj96@gmail.com>
2023-11-28 11:53:30 +0530

    practice.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "shreyash";
+const repoCount = 50;
+
+// console.log(name + repoCount + " value"); 
+console.log(`My name is ${name} and my repo count is ${repoCount}`);//  this is another type of concatenation
+
+const gameName = new String('shreyashsj');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("r"));
+
+const newStringOne = "    shreyash  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
