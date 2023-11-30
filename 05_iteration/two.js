029b35c1ecc977dfa95e81623e6ee8368857e23f
Shreyashj9600 <connectshreyashj96@gmail.com>
2023-11-30 11:30:22 +0530

    iteration.js

---
+++ 05_iteration/two.js
@@ -0,0 +1,16 @@
+// while loop practice
+
+let index = 0;
+while (index <= 10) {
+//   console.log(`value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArray = ['flash','batman','superman']
+
+let arr = 0
+while (arr < myArray.length) {
+     console.log( `value id ${myArray[arr] }`);
+     arr++
+
+}
