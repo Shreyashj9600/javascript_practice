02c9434e2ee4d96687245d5ef7f39da4c57ca491
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-16 12:30:30 +0530

    object.js

---
+++ 11_Destructuring/objects.js
@@ -0,0 +1,14 @@
+// Destructuring Objects
+
+
+let user = { 
+    name: 'rohit',
+    age: 25,
+    city: 'delhi'
+}
+
+let { name, age, city} = user;
+
+console.log(name);
+console.log(age);
+console.log(city);
