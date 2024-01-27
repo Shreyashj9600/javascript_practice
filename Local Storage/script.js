61e5a7deef33938f55c9896d78c8593123488b8b
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-28 00:00:40 +0530

    localstorage

---
+++ Local Storage/script.js
@@ -0,0 +1,12 @@
+localStorage.setItem("firstName", "shreyash");
+localStorage.setItem("location", "Delhi");
+
+localStorage.getItem("location")
+// console.log(localStorage.getItem("location") );
+
+localStorage.removeItem("location")
+
+
+sessionStorage.setItem("firstName", "shreyash");
+sessionStorage.getItem("firstName");
+console.log(sessionStorage.getItem("firstName"));
